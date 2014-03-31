a9d91807687a27668ebd7e7a192fbb9a1fac4c38
jasonbraley <jasonbraley@gmail.com>
2014-03-31 15:20:49 -0400

    Initial commit

---
+++ gen/com/example/humanrocketjlb/karaokehelper/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.humanrocketjlb.karaokehelper;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
