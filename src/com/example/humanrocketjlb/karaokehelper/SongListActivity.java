efd1dde8ecdd84c4405efd164a69811c4ed6afd5
jasonbraley <jasonbraley@gmail.com>
2014-04-05 22:36:21 -0400

    Got the search box working and reporting data to the main activity

--- src/com/example/humanrocketjlb/karaokehelper/SongListActivity.java
+++ src/com/example/humanrocketjlb/karaokehelper/SongListActivity.java
@@ -7,18 +7,23 @@ import java.util.List;
 import org.xmlpull.v1.XmlPullParserException;
 
 import android.app.ListActivity;
+import android.app.SearchManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.SearchView;
 import android.widget.Toast;
 
-public class SongListActivity extends ListActivity
+public class SongListActivity extends ListActivity implements SearchView.OnQueryTextListener,
+        SearchView.OnCloseListener
 {
     private SongListAdapter adapter;
     private List<SongRecord> songList;
-    
     private static String TAG = "KaraokeHelper";
+    private SearchView mSearchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -26,7 +31,7 @@ public class SongListActivity extends ListActivity
 	super.onCreate(savedInstanceState);
 
 	SongListParser parser = new SongListParser();
-	
+
 	try
 	{
 	    InputStream inputStream = getResources().openRawResource(R.raw.data);
@@ -41,7 +46,7 @@ public class SongListActivity extends ListActivity
 	{
 	    Log.i(TAG, "Failed to read IO");
 	}
-	
+
 	adapter = new SongListAdapter(getApplicationContext());
 	adapter.add(songList);
 	adapter.sortByArtist();
@@ -53,25 +58,52 @@ public class SongListActivity extends ListActivity
     {
 	// Inflate the menu; this adds items to the action bar if it is present.
 	getMenuInflater().inflate(R.menu.song_list, menu);
+
+	SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+	mSearchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+	mSearchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+	mSearchView.setOnQueryTextListener(this);
+	mSearchView.setOnCloseListener(this);
+
 	return true;
     }
-    
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item)
     {
 	switch (item.getItemId())
 	{
 	case R.id.action_settings:
-	    Toast.makeText(getApplicationContext(), "Nothing to see here",
-		    Toast.LENGTH_SHORT).show();
+	    Toast.makeText(getApplicationContext(), "Nothing to see here", Toast.LENGTH_SHORT).show();
 	    return true;
 	case R.id.action_search:
-	    Toast.makeText(getApplicationContext(), "Something goes here soon",
-		    Toast.LENGTH_SHORT).show();
+	    Toast.makeText(getApplicationContext(), "Something here soon", Toast.LENGTH_SHORT).show();
 	    return true;
 	default:
 	    return false;
 	}
+    }
+
+    @Override
+    public boolean onQueryTextSubmit(String query)
+    {
+	Log.i(TAG, "Got submit " + query);
+	mSearchView.clearFocus();
+	return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String newText)
+    {
+	Log.i(TAG, "Got change " + newText);
+	return false;
+    }
+
+    @Override
+    public boolean onClose()
+    {
+	Log.i(TAG, "Called close");
+	return false;
     }
 
 }
